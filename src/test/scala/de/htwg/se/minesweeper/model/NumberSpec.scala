c90dd9855920e4d77a704197c8f8749510b8164e
eFabi11 <fabian.eitel@yahoo.de>
2024-03-17 15:14:34 +0100

    tests

--- src/test/scala/de/htwg/se/minesweeper/model/NumberSpec.scala
+++ src/test/scala/de/htwg/se/minesweeper/model/NumberSpec.scala
@@ -1,43 +1,46 @@
 package de.htwg.se.minesweeper.model
 
-import de.htwg.se.minesweeper.model.Number.Num
+import java.io.{ByteArrayOutputStream, PrintStream}
 import org.scalatest.matchers.should.Matchers._
 import org.scalatest.wordspec.AnyWordSpec
+import de.htwg.se.minesweeper.model.Number.Num
 
-class NumberSpec extends AnyWordSpec {
-  "The Num function" should {
-  "correctly update the player matrix based on nearby bombs" in {
-    val side = 3
-    val testMatrix = new Matrix[Symbols](side, Symbols.Empty)
-    val bombPositions = Seq((1, 1)) // Define bomb positions
-    val testBombMatrix = bombPositions.foldLeft(testMatrix) { (matrix, pos) =>
-      matrix.replaceCell(pos._1, pos._2, Symbols.Bomb)
+class NumberSpec extends AnyWordSpec{
+    "Number" should{
+
+        val side3 = 3
+        var testMatrix = new Matrix[Symbols](side3, Symbols.Empty)
+        val testBombMatrix = testMatrix.replaceCell(1, 1, Symbols.Bomb)
+        var testPlayerMatrix = new Matrix(side3, Symbols.Covered)
+        var testAnzahlcoverd : Int = 0
+        
+        "have a Mine count" in{
+            val testTupelN = Num(0, 0, testBombMatrix, testPlayerMatrix, testAnzahlcoverd)
+                testPlayerMatrix = testTupelN._2
+                testAnzahlcoverd = testTupelN._1
+
+            testAnzahlcoverd should be (1)
+
+            val testTupelN2 = Num(0, 1, testBombMatrix, testPlayerMatrix, testAnzahlcoverd)
+                testPlayerMatrix = testTupelN2._2
+                testAnzahlcoverd = testTupelN2._1
+
+                testAnzahlcoverd should be (2)
+
+            val testTupelN3 = Num(2, 2, testBombMatrix, testPlayerMatrix, testAnzahlcoverd)
+                testPlayerMatrix = testTupelN3._2
+                testAnzahlcoverd = testTupelN3._1
+
+
+
+            testAnzahlcoverd should be (3)
+            testPlayerMatrix.cell(0,0) should be (Symbols.One)
+            testPlayerMatrix.cell(1,0) should be (Symbols.One)
+            testPlayerMatrix.cell(2,2) should be (Symbols.One)
+            testPlayerMatrix.cell(1,1) should be (Symbols.Covered)
+            testBombMatrix.cell(1,1) should be (Symbols.Bomb)
+
+
+        }
     }
-    var testPlayerMatrix = new Matrix(side, Symbols.Covered)
-    var coveredCount = 0
-
-    // Test updating a cell with one bomb nearby
-    val (updatedCoveredCount1, updatedPlayerMatrix1) = Num(0, 0, testBombMatrix, testPlayerMatrix, coveredCount)
-    withClue(s"Expected Symbols.One, but found ${updatedPlayerMatrix1.cell(0, 0)}: ") {
-      updatedPlayerMatrix1.cell(0, 0) shouldBe Symbols.One
-    }
-
-    // Suppose here you intended to test another cell, e.g., (2, 1), for the next operation
-    val (updatedCoveredCount2, updatedPlayerMatrix2) = Num(2, 1, testBombMatrix, updatedPlayerMatrix1, updatedCoveredCount1)
-    // You can add clues or assertions here as needed for this second operation
-
-    // Continuing to test with the previously updated matrix and covered count
-    val (updatedCoveredCount3, updatedPlayerMatrix3) = Num(0, 0, testBombMatrix, updatedPlayerMatrix2, updatedCoveredCount2)
-    // Add assertions for the third operation
-
-      // Testing a cell outside the valid area
-      val (updatedCoveredCount4, updatedPlayerMatrix4) = Num(-1, -1, testBombMatrix, updatedPlayerMatrix3, updatedCoveredCount3)
-      updatedCoveredCount4 shouldBe 2 // Covered count remains unchanged
-      updatedPlayerMatrix4 shouldBe updatedPlayerMatrix3 // Matrix remains unchanged
-
-      // Ensure bomb cell remains covered
-      updatedPlayerMatrix4.cell(1, 1) shouldBe Symbols.Covered
-      testBombMatrix.cell(1, 1) shouldBe Symbols.Bomb
-    }
-  }
 }
