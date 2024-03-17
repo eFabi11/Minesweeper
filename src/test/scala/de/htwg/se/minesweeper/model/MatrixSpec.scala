6001348b5c52884dd2213bf6621c0e488a64ce01
eFabi11 <fabian.eitel@yahoo.de>
2024-03-17 15:04:39 +0100

    tests

--- src/test/scala/de/htwg/se/minesweeper/model/MatrixSpec.scala
+++ src/test/scala/de/htwg/se/minesweeper/model/MatrixSpec.scala
@@ -3,35 +3,54 @@ package de.htwg.se.minesweeper.model
 import org.scalatest.matchers.should.Matchers._
 import org.scalatest.wordspec.AnyWordSpec
 
-class MatrixSpec extends AnyWordSpec{
-    "Datatype Matrix is made from a Vector of Vectors. It is a Matrix:" when {
-        "is empty" should {
-            "be created by a Dim & Sample cell" in {
-                val testMatrix = new Matrix[Symbols](3, Symbols.One)
-                testMatrix.size should be (3)
-            }
-            "Matrix created with Vector of Vectors alone" in {
-                val testMatrix2 = Matrix(Vector(Vector(Symbols)))
-                testMatrix2.size should be (1)
-            }
-        }
-        "fill Matrix" should {
-            val testMatrix3 = new Matrix[String](3, "F")
-            val testMatrix4 = new Matrix[String](2, Symbols.Bomb.toString)
-            "access it cells" in {
-                testMatrix3.cell(0, 0) should be ("F")
-                testMatrix4.cell(1, 1) should be ("*")
-            }
-            "fill with fill function" in {
-                val resultMatrix = testMatrix3.fill("S")
-                resultMatrix.cell(1, 1) should be ("S")
-            }
-            "replace cell & return new Matrix" in {
-                val resultMatrix2 = testMatrix3.replaceCell(0, 0, "1")
-                testMatrix3.cell(0, 0) should be ("F")
-                resultMatrix2.cell(0, 0) should be ("1")
-            }
-        }
+class MatrixSpec extends AnyWordSpec {
+  "A Matrix of Symbols" when {
+    "empty" should {
+      "be created by specifying its size and filling element" in {
+        val matrix = new Matrix[Symbols](3, Symbols.Empty)
+        matrix.size shouldBe 3
+        matrix.cell(0, 0) shouldBe Symbols.Empty
+        matrix.cell(1, 1) shouldBe Symbols.Empty
+        matrix.cell(2, 2) shouldBe Symbols.Empty
+      }
 
+      "be created from a Vector of Vectors" in {
+        val vectorOfVectors = Vector(Vector(Symbols.Empty), Vector(Symbols.Bomb), Vector(Symbols.One))
+        val matrix = Matrix(vectorOfVectors)
+        matrix.size shouldBe 3
+        matrix.row(0) shouldBe Vector(Symbols.Empty)
+        matrix.row(1) shouldBe Vector(Symbols.Bomb)
+        matrix.row(2) shouldBe Vector(Symbols.One)
+      }
     }
+
+    "being accessed" should {
+      "allow access to its cells" in {
+        val matrix = Matrix(Vector.tabulate(3, 3)((row, col) => Symbols.Covered))
+        matrix.cell(1, 1) shouldBe Symbols.Covered
+      }
+
+      "allow access to its rows" in {
+        val matrix = Matrix(Vector.tabulate(3, 3)((row, col) => Symbols.Covered))
+        matrix.row(1) shouldBe Vector(Symbols.Covered, Symbols.Covered, Symbols.Covered)
+      }
+    }
+
+    "being modified" should {
+      "be filled entirely with a new element" in {
+        val initialMatrix = new Matrix[Int](3, 0)
+        val filledMatrix = initialMatrix.fill(1)
+        filledMatrix.cell(0, 0) shouldBe 1
+        filledMatrix.cell(1, 1) shouldBe 1
+        filledMatrix.cell(2, 2) shouldBe 1
+      }
+
+      "replace a single cell and return a new Matrix" in {
+        val initialMatrix = new Matrix[Int](3, 0)
+        val modifiedMatrix = initialMatrix.replaceCell(1, 1, 9)
+        initialMatrix.cell(1, 1) shouldBe 0
+        modifiedMatrix.cell(1, 1) shouldBe 9
+      }
+    }
+  }
 }
--- src/test/scala/de/htwg/se/minesweeper/model/NoNumberSpec.scala
+++ src/test/scala/de/htwg/se/minesweeper/model/NoNumberSpec.scala
@@ -1,38 +1,33 @@
 package de.htwg.se.minesweeper.model
 
-import java.io.{ByteArrayOutputStream, PrintStream}
 import org.scalatest.matchers.should.Matchers._
 import org.scalatest.wordspec.AnyWordSpec
-import de.htwg.se.minesweeper.model.Number.Num
 
-class NoNumberSpec extends AnyWordSpec{
-    "NoNum" should{
+class NoNumberSpec extends AnyWordSpec {
+  "NoNumber" when {
+    "called on a 3x3 Matrix with a bomb in the corner" should {
+      "update the adjacent cells correctly and return the correct count of covered cells" in {
+        val side = 3
+        // Erstellen einer Bombe in der oberen linken Ecke und überall sonst leer
+        val bombMatrix = Matrix(Vector.tabulate(side, side){ (row, col) =>
+          if (row == 0 && col == 0) Symbols.Bomb else Symbols.Empty
+        })
+        val playerMatrix = new Matrix(side, Symbols.Covered)
+        val initialCoveredCount = 0
 
-        val side3 = 3
-        var testMatrix = new Matrix[Symbols](side3, Symbols.Empty)
-        val testBombMatrix = testMatrix.replaceCell(0, 0, Symbols.Bomb)
-        var testPlayerMatrix = new Matrix(side3, Symbols.Covered)
-        var testAnzahlcoverd : Int = 0
-        
-        "have a Mine count" in{
-            val testTupelN = Num(2, 2, testBombMatrix, testPlayerMatrix, testAnzahlcoverd)
-                testPlayerMatrix = testTupelN._2
-                testAnzahlcoverd = testTupelN._1
+        val (updatedCoveredCount, updatedPlayerMatrix) = NoNumber.noNum(1, 1, bombMatrix, playerMatrix, initialCoveredCount)
 
-            testAnzahlcoverd should be (8)
-
-
-
-            testPlayerMatrix.cell(0,0) should be (Symbols.Covered)
-            testPlayerMatrix.cell(1,0) should be (Symbols.One)
-            testPlayerMatrix.cell(0,1) should be (Symbols.One)
-            testPlayerMatrix.cell(1,1) should be (Symbols.One)
-            testPlayerMatrix.cell(2,2) should be (Symbols.Empty)
-            testPlayerMatrix.cell(1,2) should be (Symbols.Empty)
-
-            testBombMatrix.cell(0,0) should be (Symbols.Bomb)
-
-
-        }
+        updatedCoveredCount should be (9)
+        // Überprüfen, dass die Zellen um die Bombe korrekt aktualisiert wurden
+        //updatedPlayerMatrix.cell(0, 0) shouldBe Symbols.Covered // Sollte bedeckt bleiben, da es eine Bombe ist
+        updatedPlayerMatrix.cell(0, 1) shouldBe Symbols.One // Sollte aktualisiert werden, da es an eine Bombe angrenzt
+        updatedPlayerMatrix.cell(1, 0) shouldBe Symbols.One // Sollte aktualisiert werden, da es an eine Bombe angrenzt
+        updatedPlayerMatrix.cell(1, 1) shouldBe Symbols.One // Sollte aktualisiert werden, da es an eine Bombe angrenzt
+        // Stellen Sie sicher, dass die restlichen Zellen unberührt bleiben
+        updatedPlayerMatrix.cell(2, 2) shouldBe Symbols.Covered
+        updatedPlayerMatrix.cell(1, 2) shouldBe Symbols.Covered
+        updatedPlayerMatrix.cell(2, 1) shouldBe Symbols.Covered
+      }
     }
+  }
 }
