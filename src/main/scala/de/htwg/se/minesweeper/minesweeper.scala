10cee6b023da4b75a9a2cd9d8ae443e3eea3f2c3
eFabi11 <fabian.eitel@yahoo.de>
2024-04-05 11:42:39 +0200

    test

--- src/main/scala/de/htwg/se/minesweeper/minesweeper.scala
+++ src/main/scala/de/htwg/se/minesweeper/minesweeper.scala
@@ -1,25 +1,28 @@
 package de.htwg.se.minesweeper
 
-import model.{Field, Matrix, Symbols, Game}
+import de.htwg.se.minesweeper.model.{Field, Matrix, Symbols, Game, Status}
 import de.htwg.se.minesweeper.controller.Controller
 import de.htwg.se.minesweeper.aview.TUI
-import de.htwg.se.minesweeper.model.Status
 import de.htwg.se.minesweeper.util.StdInInputSource
+import de.htwg.se.minesweeper.difficulty.DifficultyLevel
 
 object Minesweeper {
-  def main(args: Array[String]): Unit = {
-    var msGame = new Game(Status.Playing)
-
-    val diff = msGame.setDifficulty()
-    val side = diff._1
-    val bombs = diff._2
-    msGame.anzahBomben = bombs
-    msGame.side = side
-
-    var coveredField = new Field(side, Symbols.Covered)
-    val controller = Controller(coveredField, msGame)
-    val inputSource = StdInInputSource
-    val tui = new TUI(controller, inputSource)
-    tui.run()
-  }
+    def main(args: Array[String]): Unit = {
+        val msGame = new Game(DifficultyLevel.Easy)
+        val gridSize = 10 // Oder eine andere Logik, um die Größe basierend auf der Schwierigkeitsstufe zu bestimmen
+        val controller = new Controller(new Field(gridSize, Symbols.Covered), msGame) // Korrekt
+        val inputSource = StdInInputSource
+        val tui = new TUI(controller, inputSource)
+        
+        // Erste Zugkoordinaten vom Benutzer holen
+        val (startX, startY) = tui.getFirstMoveCoordinates()
+        
+        // Spielfeld mit den Koordinaten des ersten Zuges initialisieren
+        val coveredField = msGame.initializeField(startX, startY)
+        
+        // Controller und TUI mit dem aktualisierten Feld initialisieren
+        controller.field = coveredField
+        
+        tui.run()
+    }
 }
